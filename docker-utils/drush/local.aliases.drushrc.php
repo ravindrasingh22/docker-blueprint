c6c57c3c9c1b407dc07fea6493d825a8aa1c04aa
Pavan Keshavamurthy <pavan.murthy@srijan.net>
2016-12-07 16:49:51 +0530

    generalize aliases

---
+++ docker-utils/drush/local.aliases.drushrc.php
@@ -0,0 +1,22 @@
+<?php
+
+if (!isset($drush_major_version)) {
+  $drush_version_components = explode('.', DRUSH_VERSION);
+  $drush_major_version = $drush_version_components[0];
+}
+// Site devportal2, environment dev
+$aliases['local-dev'] = array(
+  'root' => '/var/www/html/docroot',
+  'uri' => 'dev.srijan.local',
+  'remote-host' => 'dev.srijan.local',
+  'remote-user' => 'root',
+  //'ssh-options' => '-p 49100',
+);
+
+$aliases['local-dev.ssh'] = array(
+  'root' => '/var/www/html/docroot',
+  'uri' => 'dev.srijan.local',
+  'remote-host' => 'dev.srijan.local',
+  'remote-user' => 'root',
+  'ssh-options' => '-p 49100',
+);
